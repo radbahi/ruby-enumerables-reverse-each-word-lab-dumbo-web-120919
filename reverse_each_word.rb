3b33949130959d99a1a22e7736774afda5537131
Radouane Bahi <radbahi@gmail.com>
2019-12-04 01:56:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ splitStringArray.each do |string|
 end
 newArray.join(" ")
 end
+
+def reverse_each_word_collect(string)
+  splitStringArray = string.split(" ")
+  newArray = [] 
+  splitStringArray.collect do |string|
+    newArray << string.reverse 
+  end
+  newArray.join(" ")
+end 
